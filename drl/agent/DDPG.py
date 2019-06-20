2ee44c5ed12473e1b1f367c9174cb5f59089fbcd
Roman Wiatr <rwiatr@gmail.com>
2019-06-20 16:24:52 +0200

    fixed some layers

--- drl/agent/DDPG.py
+++ drl/agent/DDPG.py
@@ -38,24 +38,62 @@ class Agent(BaseAgent):
             self.learn(sample)
 
     def learn(self, experiences):
-        states, actions, rewards, next_states, dones = experiences  # unmap(experiences, self.conf.s_dim, self.conf.a_dim)
+        # states, actions, rewards, next_states, dones = experiences  # unmap(experiences, self.conf.s_dim, self.conf.a_dim)
+        #
+        # # ### UPDATE CRITIC LOCAL ### #
+        # next_actions = self.target.actor(next_states)  # u'(s_t+1) = ~a_t+2
+        # # calculate the prediction for the next_Q_targets (cumulative reward from next step)
+        # next_q_targets = self.target.critic(next_states, next_actions)  # Q'(s_t+1, ~a_t+2)
+        # # cumulative reward from this step is reward + discounted cumulative reward from next step (next_Q_targets)
+        # q_targets = rewards + (self.conf.gamma * next_q_targets * (1 - dones))
+        #
+        # # Q(s_t, a_t)
+        # expected_q_targets = self.local.critic(states, actions)
+        # # Optimizing local critic using Q(s_t, a_t) - R + gamma * Q'(s_t+1, u'(s_t+1)) by minimising the loss
+        # self.local.learn_critic(F.mse_loss(expected_q_targets, q_targets))
+        #
+        # # ### UPDATE ACTOR LOCAL ### #
+        # # a_t+1 <- u(s_t)
+        # actions = self.local.actor(states)
+        # # -mean(Q(s_t, a_t+1))
+        # self.local.learn_actor(-self.local.critic(states, actions).mean())
+        #
+        # self.target.update(self.local, self.conf.tau)
 
-        # ### UPDATE CRITIC LOCAL ### #
-        next_actions = self.target.actor(next_states)  # u'(s_t+1) = ~a_t+2
-        # calculate the prediction for the next_Q_targets (cumulative reward from next step)
-        next_q_targets = self.target.critic(next_states, next_actions)  # Q'(s_t+1, ~a_t+2)
-        # cumulative reward from this step is reward + discounted cumulative reward from next step (next_Q_targets)
-        q_targets = rewards + (self.conf.gamma * next_q_targets * (1 - dones))
+        states, actions, rewards, next_states, dones = experiences
 
-        # Q(s_t, a_t)
-        expected_q_targets = self.local.critic(states, actions)
-        # Optimizing local critic using Q(s_t, a_t) - R + gamma * Q'(s_t+1, u'(s_t+1)) by minimising the loss
-        self.local.learn_critic(F.mse_loss(expected_q_targets, q_targets))
+        # ---------------------------- update critic ---------------------------- #
+        # Get predicted next-state actions and Q values from target models
+        # actions_next = self.actor_target(next_states)
+        actions_next = self.target.actor(next_states)
+        # Q_targets_next = self.critic_target(next_states, actions_next)
+        Q_targets_next = self.target.critic(next_states, actions_next)
+        # Compute Q targets for current states (y_i)
+        Q_targets = rewards + (self.conf.gamma * Q_targets_next * (1 - dones))
+        # Compute critic loss
+        # Q_expected = self.critic_local(states, actions)
+        Q_expected = self.local.critic(states, actions)
+        critic_loss = F.mse_loss(Q_expected, Q_targets)
+        # Minimize the loss
+        # self.critic_optimizer.zero_grad()
+        # critic_loss.backward()
+        # self.critic_optimizer.step()
+        self.local.learn_critic(critic_loss)
 
-        # ### UPDATE ACTOR LOCAL ### #
-        # a_t+1 <- u(s_t)
-        actions = self.local.actor(states)
-        # -mean(Q(s_t, a_t+1))
-        self.local.learn_actor(-self.local.critic(states, actions).mean())
+        # ---------------------------- update actor ---------------------------- #
+        # Compute actor loss
+        # actions_pred = self.actor_local(states)
+        actions_pred = self.local.actor(states)
+        # actor_loss = -self.critic_local(states, actions_pred).mean()
+        actor_loss = -self.local.critic(states, actions_pred).mean()
+        # Minimize the loss
+        # self.actor_optimizer.zero_grad()
+        # actor_loss.backward()
+        # self.actor_optimizer.step()
+        self.local.learn_actor(actor_loss)
 
+        # ----------------------- update target networks ----------------------- #
+        # self.soft_update(self.critic_local, self.critic_target, TAU)
+        # self.soft_update(self.actor_local, self.actor_target, TAU)
+        # self.soft_update(self.local, self.target, TAU)
         self.target.update(self.local, self.conf.tau)
