247e21bbb115c3f0c20484eb9468d177f25fcf7c
Roman Wiatr <rwiatr@gmail.com>
2019-06-21 18:50:59 +0200

    fixed some layers

--- drl/agent/SharedMem.py
+++ drl/agent/SharedMem.py
@@ -14,24 +14,44 @@ def multi_ddpg_with_shared_mem(conf, n):
 
 
 class Agent(BaseAgent):
-    def __init__(self, conf, agents):
+    def __init__(self, conf, agents, update_every_n_steps=None):
         super().__init__(conf)
         self.memory = ReplayBuffer(conf.buffer_size, conf.s_dim, conf.a_dim, conf.seed)
         self.agents = agents
+        self.update_every_n_steps = update_every_n_steps
+        self.step = 0
 
     def act(self, states):
         return list(a.act(s) for a, s in zip(self.agents, states))
 
     def step(self, states, actions, rewards, next_states, dones):
+        self.step += 1
         for idx in range(len(self.agents)):
             self.memory.add(states[idx], actions[idx], rewards[idx], next_states[idx], dones[idx])
 
+        if not self.update_every_n_steps:
+            self.try_update()
+        elif self.step > self.update_every_n_steps:
+            self.try_update()
+            self.step = 0
+
+    def try_update(self):
         if len(self.memory) > self.conf.batch_size:
             for agent in self.agents:
-                sample = self.memory.sample(self.conf.batch_size)
-                sample = torch.from_numpy(sample).to(self.conf.device)
+
                 if isinstance(agent, ddpg_agent.Agent):
-                    sample = unmap(sample, self.conf.s_dim, self.conf.a_dim)
+                    sample = self.memory.sample(self.conf.batch_size)
+                    states, actions, rewards, next_states, dones = unmap(sample, self.conf.s_dim, self.conf.a_dim)
+                    sample = (
+                        torch.from_numpy(states).to(self.conf.device),
+                        torch.from_numpy(actions).to(self.conf.device),
+                        torch.from_numpy(rewards).to(self.conf.device),
+                        torch.from_numpy(next_states).to(self.conf.device),
+                        torch.from_numpy(dones).to(self.conf.device)
+                    )
+                else:
+                    sample = self.memory.sample(self.conf.batch_size)
+                    sample = torch.from_numpy(sample).to(self.conf.device)
                 agent.learn(sample)
 
     def learn(self, experiences):
