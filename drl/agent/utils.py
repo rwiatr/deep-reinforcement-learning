dfd0ff8e78e756a507d27f6aeb7fb1abff73b7f6
Roman Wiatr <rwiatr@gmail.com>
2019-06-23 22:08:14 +0200

    fixed some layers

--- drl/agent/utils.py
+++ drl/agent/utils.py
@@ -48,39 +48,6 @@ class OUNoise:
         return self.state
 
 
-class ReplayBuffer3:
-
-    def __init__(self, buffer_size, seed, device):
-        # self.action_size = action_size
-        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
-        self.device = device
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed = random.seed(seed)
-
-    def add(self, state, action, reward, next_state, done):
-        """Add a new experience to memory."""
-        e = self.experience(state, action, reward, next_state, done)
-        self.memory.append(e)
-
-    def sample(self, batch_size):
-        """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=batch_size)
-
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(self.device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(self.device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(
-            self.device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(
-            self.device)
-
-        return (states, actions, rewards, next_states, dones)
-
-    def __len__(self):
-        """Return the current size of internal memory."""
-        return len(self.memory)
-
-
 class ReplayBuffer2:
     def __init__(self, buffer_size, seed, device):
         self.device = device
