563792fc7413dc610b7dae64f3d1ce0a83ec12ab
Roman Wiatr <rwiatr@gmail.com>
2019-06-23 17:52:02 +0200

    fixed some layers

--- drl/network/body.py
+++ drl/network/body.py
@@ -52,6 +52,22 @@ def vanilla_action_fc_net(s_dim, a_dim, fc_units=(0, 300, 150, 1), actv=(F.relu,
     return ActionFCNet(l_dims=tuple(fc_units), actv=actv, a_dim=a_dim, action_cat=action_cat, seed=seed)
 
 
+class ActionFCNet2(nn.Module):
+
+    def __init__(self, state_size, action_size, seed, fcs1_units=300, fc2_units=150):
+        super(ActionFCNet2, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        self.fcs1 = init_layer(nn.Linear(state_size, fcs1_units))
+        self.fc2 = init_layer(nn.Linear(fcs1_units + action_size, fc2_units))
+        self.fc3 = init_layer(nn.Linear(fc2_units, 1))
+
+    def forward(self, state, action):
+        xs = F.relu(self.fcs1(state))
+        x = torch.cat((xs, action), dim=1)
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+
+
 class ActionFCNet(nn.Module):
     """
     l_dims = list of layer dimensions
